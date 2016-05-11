8ce135598fc577fe6d4c05bce71d056bbf06350d
Steve <vtec9k@gmail.com>
2016-05-11 17:28:52 +0100

    Add files via upload

---
+++ Check-Svc-Paths.ps1
@@ -0,0 +1,12 @@
+﻿#Pull list of services from registry
+
+$svclist = Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\services | ForEach-Object {Get-ItemProperty $_.PsPath}
+
+#Ignore anything after .exe, filter for vulnerabile services
+ForEach ($svc in $svclist) {
+    $svcpath = $svc.ImagePath -split ".exe"
+    if(($svcpath[0] -like "* *") -and ($svcpath[0] -notlike '"*') -and ($svcpath[0] -notlike "\*")) {
+        $svc | fl -Property DisplayName,ImagePath,PsPath
+    }
+}
+
