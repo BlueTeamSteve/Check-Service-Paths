e15c10f1ad2e8a3f5103e08b881a62192dc2d6ac
Steve <vtec9k@gmail.com>
2017-04-01 10:34:58 +0100

    Permission check
    Added permission check

--- Check-Service-Paths.ps1
+++ Check-Service-Paths.ps1
@@ -2,15 +2,13 @@
 
 $svclist = Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\services | ForEach-Object {Get-ItemProperty $_.PsPath}
 
-#Ignore anything after .exe, filter for vulnerabile services
+#Ignore anything after .exe, filter for vulnerable services
 ForEach ($svc in $svclist) {
     $svcpath = $svc.ImagePath -split ".exe"
     if(($svcpath[0] -like "* *") -and ($svcpath[0] -notlike '"*') -and ($svcpath[0] -notlike "\*")) {
         $svc | fl -Property DisplayName,ImagePath,PsPath
+
+        #Check service permissions (Full Control or Modify is BAD!!)
+        Get-Acl $svc.ImagePath | fl
     }
-
-#Check service permissions (Full Control or Modify is BAD!!)
-
-
-
 }
